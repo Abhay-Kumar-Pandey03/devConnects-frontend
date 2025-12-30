7ddb40e469fa2cecd879719c350084610afdf1b5
Abhay Kumar Pandey <abhaypandey7067@gmail.com>
2025-12-30 21:26:56 +0530

    minor changes in login

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,47 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Provider, useSelector } from "react-redux";
+import appStore from "./utils/appStore";
+
 import Body from "./components/Body";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
 import Feed from "./components/Feed";
-import { Provider } from "react-redux";
-import appStore from "./utils/appStore";
 import Connections from "./components/Connections";
 import Requests from "./components/Requests";
+
+function AppRoutes() {
+  const user = useSelector((store) => store.user);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Body />}>
+
+        {/* ✅ ONLY THIS ROUTE CONTROLS REDIRECT */}
+        <Route
+          index
+          element={
+            user
+              ? <Navigate to="/feed" replace />
+              : <Navigate to="/login" replace />
+          }
+        />
+
+        <Route path="login" element={<Login />} />
+        <Route path="feed" element={<Feed />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="connections" element={<Connections />} />
+        <Route path="requests" element={<Requests />} />
+
+      </Route>
+    </Routes>
+  );
+}
 
 export default function App() {
   return (
     <Provider store={appStore}>
-      <BrowserRouter basename = "/">
-      <Routes>
-
-        <Route path = "/" element = {<Body />} >
-
-        <Route index element={<Feed />} />
-
-        <Route path = "/login" element = {<Login />} />
-        <Route path = "/profile" element = {<Profile />} />
-        <Route path = "/connections" element = {<Connections />} />
-        <Route path = "/requests" element = {<Requests />} />
-
-        </Route>
-
-      </Routes>
+      <BrowserRouter>
+        <AppRoutes />
       </BrowserRouter>
     </Provider>
   );
